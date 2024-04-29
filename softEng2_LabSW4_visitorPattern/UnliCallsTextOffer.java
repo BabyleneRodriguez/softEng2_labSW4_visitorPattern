de7f484a3e041a7029f03f5916743e5ed8383ed9
Babylene Rodriguez <142875371+BabyleneRodriguez@users.noreply.github.com>
2024-04-29 11:04:41 +0800

    Delete softEng2_LabSW4_visitorPattern directory

--- softEng2_LabSW4_visitorPattern/UnliCallsTextOffer.java
+++
@@ -1,7 +0,0 @@
-package softEng2_LabSW4_visitorPattern;
-
-public interface UnliCallsTextOffer
-{
-    public String showUnliCallsTextOffer(String telcoName, boolean unliCallText);
-    public String showUnliCallsTextOffer(TelcoSubscription telco);
-}
--- softEng2_LabSW4_visitorPattern/UsagePromo.java
+++
@@ -1,7 +0,0 @@
-package softEng2_LabSW4_visitorPattern;
-
-public interface UsagePromo
-{
-    public String showAllowance(String telcoName, double money);
-    public String showAllowance(TelcoSubscription telco);
-}
